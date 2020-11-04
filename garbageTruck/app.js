7c757401fd9e02ac97acc59908e29c7238a67882
Jack Swayze <talk2dug@gmail.com>
2020-11-03 23:17:57 -0500

    making changes

--- garbageTruck/app.js
+++ garbageTruck/app.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var GPSarray = []
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var GPS = require('./node_modules/gps/gps.js');
-var app = express();
 var prevLAT;
 var prevLON;
 var gpsTime;
@@ -18,57 +10,30 @@ var gpsCord = "comment=lat:"+gpsLat +",lon:"+gpsLat
 var moment = require('moment');
 const geolib = require('geolib');
 var distenceFromBase;
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 var record = 0;
 var gps = new GPS;
-    const SerialPort = require('serialport')
-    const port = new SerialPort('/dev/ttyACM0', {
-        baudRate: 9600
-    })
-  
-    const Readline = require('@serialport/parser-readline')
+const SerialPort = require('serialport')
+const port = new SerialPort('/dev/ttyACM0', {
+    baudRate: 9600
+})
+const Readline = require('@serialport/parser-readline')
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
+
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', function(data) {
     
-    // Open errors will be emitted as an error event
-    port.on('error', function(err) {
-      console.log('Error: ', err.message)
-    })
- 
-    const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
-    parser.on('data', function(data) {
-
-        //console.log(data)
-        gps.update(data);
-        //parseGPS(gps);
-    })
-
+    gps.update(data);
+    //parseGPS(gps);
+})
   
     var timeStamp
     function calculateHeading(lon, lat) {
@@ -82,6 +47,7 @@ var gps = new GPS;
     }
     gps.on('GGA', function(data) {
         gpsTime = data.time
+        
          timeStamp = moment(gpsTime).format("YYYY-MM-DD-HH-mm-ss")
         var headingDir = calculateHeading(data.lon, data.lat)
         gpsLon = data.lon
@@ -91,7 +57,7 @@ var gps = new GPS;
         if (gps.state.speed == null) {gpsSpeed = 0}
          distenceFromBase = geolib.getDistance(
             { latitude: gpsLat, longitude: gpsLon },
-            { latitude: 38.926397, longitude: -77.0963497 },
+            { latitude: 38.9262652, longitude: -75.6958141 },
             0.01
         )
         distenceFromBase = distenceFromBase*0.00062
@@ -101,7 +67,7 @@ var gps = new GPS;
          GPSData = "{'lat':"+gpsLat +",'lon':"+gpsLon +",'gpsTime':"+ timeStamp +"},\r\n"
          fs.appendFile( "../GPS.txt", GPSData, (err) => {
             if (err) console.log(err);
-            //console.log("Successfully Written GPS to File.");
+            console.log("Successfully Written GPS to File.");
         });  
     }  
     gpsCord = "comment={'lat':"+gpsLat +",'lon':"+gpsLon +",'gpsTime':"+ timeStamp +"}"
@@ -120,7 +86,7 @@ var gps = new GPS;
                 "-r", "24",
                "-f", "v4l2", 
                
-               "-video_size", "1280x720", 
+               "-video_size", "1920x1080", 
                "-input_format", 
                "mjpeg", "-i", "/dev/video0", "-metadata", gpsCord,
                "-c", "copy", "-t", "30",name
@@ -136,7 +102,7 @@ var gps = new GPS;
                 "-r", "24",
                "-f", "v4l2", 
                
-               "-video_size", "1280x720", 
+               "-video_size", "1920x1080", 
                "-input_format", 
                "mjpeg", "-i", "/dev/video2", "-metadata", gpsCord, 
                "-c", "copy", "-t", "30", name2
@@ -171,10 +137,7 @@ var gps = new GPS;
             
                 startRecording()
                 
-            }, 32000);
+            }, 10000);
             
-          
-              
-          
-module.exports = app;
+ 
 
